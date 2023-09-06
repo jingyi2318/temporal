2a2b4c1e18dcc26b6b475deeaf6ddb166176b2f1
Jing Yi Wang <jingyi.wang@datadoghq.com>
2023-09-06 15:31:00 -0400

    wip

--- temporal/fx.go
+++ temporal/fx.go
@@ -237,13 +237,13 @@ func ServerOptionsProvider(opts []ServerOption) (serverOptionsProvider, error) {
 
 		if esClient != nil {
 			logger.Error(fmt.Sprintf("esConfig: %s, %s, %v", esConfig.Version, esConfig.Username))
-			if esClient.AWSRequestSigning != nil {
-				logger.Error(fmt.Sprintf("esConfig: %v", esConfig.Version, esConfig.Username, esConfig.AWSRequestSigning))
-			} else {
-				logger.Error("esCofig.AWSRequestSigning is nil")
-			}
+			// if esClient.AWSRequestSigning != nil {
+			// 	logger.Error(fmt.Sprintf("esConfig: %v", esConfig.Version, esConfig.Username, esConfig.AWSRequestSigning))
+			// } else {
+			// 	logger.Error("esCofig.AWSRequestSigning is nil")
+			// }
 		} else {
-			logger.Error("esCofig is nil")
+			logger.Error("esConfig is nil")
 		}
 
 		logger.Warn("jing yi test 123")
